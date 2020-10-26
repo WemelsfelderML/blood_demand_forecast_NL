59cf3d5702a0f7e53c75e6cdf4c81d7403d35be0
Mikko Arvas <mikko.arvas@veripalvelu.fi>
2020-10-26 11:32:14 +0200

    rw merge.months and method.select to command line

--- errors_allyears_graphs.R
+++ errors_allyears_graphs.R
@@ -1,3 +1,10 @@
+#cat errors_allyears_graphs.R | R --slave --args 3 6 l
+a <- commandArgs()
+print(a)
+#$ cat errors_allyears_graphs.R | R --slave --args 5 6
+#[1] "/usr/lib/R/bin/exec/R" "--slave"               "--args"               
+#[4] "5"  "6" "l"
+
 library(hash)
 library(RColorBrewer)
 library(stringr)
@@ -25,17 +32,38 @@ if (NL) {
   ROOTDIR <- "/home/merel/Documents/Sanquin/blood_demand_forecast_NL/"
   groups <- c("RED", "Ominus", "Oplus", "Aminus", "Aplus", "Bminus", "Bplus", "ABminus", "ABplus", "PLAT")
 } else {
-  ROOTDIR <- "~/Work/proj/OPERATIONAL/blood_demand_forecast_NL/"
+  ROOTDIR <- "~/Work/proj/OPERATIONAL/blood_demand_forecast_NL/20201023/"
   groups <- c("RED")
 }
 
 period <- "m"               # m for monthly, w for weakly
-rw <- 3
-method.select <- c("snaive", "5-MA", "7-MA", "9-MA", "12-MA", "stl", "ets", "tbats", "stlf", "arimax", "dynreg", "nn", "combined")
-# method.select <- c("stlf", "7-MA", "ets", "nn", "dynreg", "tbats")        # monthly
-#method.select <- c("7-MA", "ets","tbats", "stlf", "dynreg", "nn")        # monthly FIN 20201023
-# method.select <- c("combined", "snaive", "stl", "5-MA", "ets", "dynreg")    # weekly
+rw <- 5
+if (!is.na(commandArgs()[4])) {
+  rw <- as.numeric(commandArgs()[4])
+}
+
 merge.months = 6
+if (!is.na(commandArgs()[5])) {
+  merge.months <- as.numeric(commandArgs()[5])
+}
+
+# method.select <- c("snaive", "5-MA", "7-MA", "9-MA", "12-MA", "stl", "ets", "tbats", "stlf", "arimax", "dynreg", "nn", "combined")
+#method.select <- c("stlf", "7-MA", "ets", "nn", "dynreg", "tbats")        # monthly
+# method.select <- c("combined", "snaive", "stl", "5-MA", "ets", "dynreg")    # weekly
+method.select <- c("7-MA", "ets","tbats", "stlf", "dynreg", "nn")        # monthly
+
+if (!is.na(commandArgs()[6])) {
+  merge.param <- commandArgs()[6]
+  if (merge.param == "l") {
+    method.select <- c("snaive", "5-MA", "7-MA", "9-MA", "12-MA", "stl", "ets", "tbats", "stlf", "arimax", "dynreg", "nn", "combined")
+  }
+  if (merge.param == "s") {
+    method.select <- c("7-MA", "ets","tbats", "stlf", "dynreg", "nn")
+  }
+}
+
+
+# DATA PROCESSING
 
 # reshape error data frames and put them together in a hash set with rolling window size as key
 df <- read.delim(file = paste0(ROOTDIR, "rw_testing/", period, "_errors_rwy", toString(rw), "_all.txt"), header = FALSE, sep = ";")
@@ -43,7 +71,7 @@ df <- read.delim(file = paste0(ROOTDIR, "rw_testing/", period, "_errors_rwy", to
 #> df$V1 %in% method.select
 #[1] FALSE FALSE FALSE  TRUE FALSE FALSE FALSE  TRUE  TRUE  TRUE FALSE  TRUE  TRUE FALSE
 #This does not guarantee that they would be in the same order
-# arvasmi@tk-arvasmi:~/Work/proj/OPERATIONAL/blood_demand_forecast_NL/rw_testing$ cut -d ";" -f1 m_errors_rwy3_all.txt 
+# arvasmi rw_testing$ cut -d ";" -f1 m_errors_rwy3_all.txt 
 # Red
 # snaive
 # 5-MA
@@ -58,25 +86,8 @@ df <- read.delim(file = paste0(ROOTDIR, "rw_testing/", period, "_errors_rwy", to
 # dynreg
 # nn
 # combined
-# > cbind(df[df$V1 %in% method.select,1:2],method.select)
-# V1        V2 method.select
-# 4    7-MA 7.2527823          stlf
-# 8     ets 6.0121560          7-MA
-# 9   tbats 1.2525924           ets
-# 10   stlf 0.8383647            nn
-# 12 dynreg 3.2047730        dynreg
-# 13     nn 5.9707662         tbats
-#Put method.select <- c("stlf", "7-MA", "ets", "nn", "dynreg", "tbats")        # monthly
-#in the same order that they are in the file. IS THIS THE SAME IN FIN AND NL???
-#cbind(df[df$V1 %in% method.select,1:2],method.select)
-# > cbind(df[df$V1 %in% method.select,1:2],method.select)
-# V1        V2 method.select
-# 4    7-MA 7.2527823          7-MA
-# 8     ets 6.0121560           ets
-# 9   tbats 1.2525924         tbats
-# 10   stlf 0.8383647          stlf
-# 12 dynreg 3.2047730        dynreg
-# 13     nn 5.9707662            nn
+
+
 df <- df[df$V1 %in% method.select,]
 rownames(df) <- NULL
 
@@ -87,9 +98,11 @@ if (NL) {
   #This is only used to get the max value
 } else {
   d <- data.frame(year=2020)
-  #Finish data extends to 2020.06. Remove last 6 timepoints
-  df <- df[,- ((ncol(df) -6) : ncol(df))]
+  #Finish data extends to 2020.06, predictions extend to 2020.05. Hence, remove last 5 timepoints to make error data extend to 2019.12.01
+  df <- df[,- ((ncol(df) -5) : ncol(df))]
 }
+
+
 
 # number of methods tested for each blood group
 m <- length(method.select)
@@ -168,6 +181,7 @@ if (NL) {
   df.plot <- as_tibble(df.group)
 }
 names(colors) <- method.select
+#colnames(df.plot) <- seq((max(d$year)-(n*(merge.months/p))+1), (max(d$year)+1-(merge.months)/p), (merge.months/p))
 colnames(df.plot) <- c("Method",seq((max(d$year)-(n*(merge.months/p))+1), (max(d$year)+1-(merge.months)/p), (merge.months/p)))
 #df.plot$Method <- method.select
 df.plot <- df.plot %>% pivot_longer(cols= !Method) %>% mutate(Year=as.numeric(name))
@@ -178,20 +192,21 @@ gr <- gr + scale_colour_manual(name = "Method",values = colors) + ylab("Predicti
 gr <- gr + theme_bw(base_size = 18)
 gr <- gr + theme(legend.position = "bottom", legend.direction = "horizontal")
 if (NL) {
-  filename <- paste0(ROOTDIR, "rw_testing/img/all_years/", period, "_rwy", toString(rw), "_",length(method.select) ,"_red", "_gg.pdf")
+  filename <- paste0(ROOTDIR, "rw_testing/img/all_years/", period, "_rwy", rw, "_m",merge.months, "_red", "_gg.pdf")
 } else {
-  filename <- paste0(ROOTDIR, "rw_testing/img/all_years/", period, "_rwy", toString(rw), "_",length(method.select) ,"_red", "_gg_FIN.pdf")
+  filename <- paste0(ROOTDIR, "rw_testing/img/all_years/", period, "_rwy", rw, "_m",merge.months, "_red", "_gg_FIN.pdf")
 }
 ggsave(filename=filename, gr, width = 180,  height = 180,units="mm", dpi=600, scale=1.0)
 
-gr <- gr + facet_wrap(~Method)
+gr <- gr + facet_wrap(~Method) +  theme(axis.text.x = element_text(angle = 90)) 
 
 if (NL) {
-  filename <- paste0(ROOTDIR, "rw_testing/img/all_years/", period, "_rwy", toString(rw), "_",length(method.select) ,"_red", "_gg_f.pdf")
+  filename <- paste0(ROOTDIR, "rw_testing/img/all_years/", period, "_rwy", rw, "_m",merge.months,"_l",length(method.select) ,"_red", "_gg_f.pdf")
 } else {
-  filename <- paste0(ROOTDIR, "rw_testing/img/all_years/", period, "_rwy", toString(rw), "_",length(method.select) ,"_red", "_gg_f_FIN.pdf")
+  filename <- paste0(ROOTDIR, "rw_testing/img/all_years/", period, "_rwy", rw, "_m",merge.months,"_l",length(method.select) ,"_red", "_gg_f_FIN.pdf")
 }
 ggsave(filename=filename, gr, width = 360,  height = 450,units="mm", dpi=600, scale=1.0)
+
 
 #Another way to count....
 # d2 <- read.delim(file = paste0(ROOTDIR, "rw_testing/", period, "_errors_rwy", toString(rw), "_all.txt"), header = FALSE, sep = ";")
