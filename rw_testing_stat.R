4291d598e4879e6e75581d1c691f472cc60fca41
Mikko Arvas <mikko.arvas@veripalvelu.fi>
2020-10-27 15:07:00 +0200

    fixing t-test window counts to accomodate both data sources

--- rw_testing_stat.R
+++ rw_testing_stat.R
@@ -6,17 +6,26 @@ library(ggplot2)
 library(ggpubr)
 
 # SETTINGS
+NL <- FALSE
+#NL <- TRUE
+
 
 # working directory
-ROOTDIR <- "/home/merel/Documents/Sanquin/blood_demand_forecast_NL/"
+if (NL) {
+  ROOTDIR <- "/home/merel/Documents/Sanquin/blood_demand_forecast_NL/"
+  groups <- c("RED", "Ominus", "Oplus", "Aminus", "Aplus", "Bminus", "Bplus", "ABminus", "ABplus", "PLAT")
+} else {
+  ROOTDIR <- "~/Work/proj/OPERATIONAL/blood_demand_forecast_NL/20201026/"
+  groups <- c("RED","PLAT")
+}
+
 
 # "avg" for taking average result of all methods, 
 # "best" for taking only result of best performing method
-period <- "w"               # m for monthly, w for weakly
-groups <- c("RED", "O-", "O+", "A-", "A+", "B-", "B+", "AB-", "AB+", "PLAT")
+period <- "m"               # m for monthly, w for weakly
 
 # list of all considered rolling window sizes (years)
-rolling_windows <- c(3:8)
+rolling_windows <- c(3:10)
 
 # put everything in a function in order to efficiently execute for each blood group
 fun <- function(method_avg_best, period, group, rolling_windows){
@@ -25,9 +34,11 @@ fun <- function(method_avg_best, period, group, rolling_windows){
   
   # read errors of first rolling window to determine the set of used models
   df <- read.delim(file = paste0(ROOTDIR, "rw_testing/", period, "_errors_rwy", toString(rolling_windows[1]), "_all.txt"), header = FALSE, sep = ";")
-  index_start <- match(group, toupper(df$V1)) + 1
-  index_stop <- index_start + nrow(df)/10 - 2
-  used_models <- df[c(index_start:index_stop),]$V1
+  index_start <- match(groups[1], toupper(df$V1)) + 1
+  #index_stop <- index_start + nrow(df)/10 - 2
+  index_stop <- index_start + 12
+  used_models <- as.character(df[c(index_start:index_stop),]$V1)
+  
   
   # initialize data frame with all prediction errors
   if (period == "m"){
@@ -40,10 +51,12 @@ fun <- function(method_avg_best, period, group, rolling_windows){
   for (rw in rolling_windows) {
     df <- read.delim(file = paste0(ROOTDIR, "rw_testing/", period, "_errors_rwy", toString(rw), "_all.txt"), header = FALSE, sep = ";")
     index_start <- match(group, toupper(df$V1)) + 1
-    index_stop <- index_start + nrow(df)/10 - 2
-    df <- df[c(index_start:index_stop),]
+    #index_stop <- index_start + nrow(df)/10 - 2
+    index_stop <- index_start + 12
+    #df <- df[c(index_start:index_stop),]
+    df <- df[c(index_start:index_stop),c(1,(ncol(df) - 10) : ncol(df))]
     df <- df[df$V1 %in% used_models,]
-    
+    cat(group,rw,dim(df),"\n")
     ls <- list()
     for (r in rownames(df)){
       ls <- append(ls, as.vector(t(df[r,c(2:ncol(df))])))
@@ -90,3 +103,4 @@ fun <- function(method_avg_best, period, group, rolling_windows){
 for (group in groups) {
   fun(method_avg_best, period, group, rolling_windows)
 }
+
